4cd521c5d526d4ce460246d496fc6917e61d0200
smartappsgh <smart.trader.dev@gmail.com>
2024-05-15 16:03:52 +0300

    add: footer

--- app/components/Footer/Footer.tsx
+++ app/components/Footer/Footer.tsx
@@ -1,7 +1,26 @@
 import React from 'react'
+import './FooterStyled'
+import logo from '../../images/logo.png'
+import Image from 'next/image'
+import { FooterContainer } from './FooterStyled'
 
 export const Footer = () => {
   return (
-    <div>Footer</div>
+    <div className='container'>
+      <FooterContainer>
+      <div className='footer__logo'>
+          <Image src={logo} alt='logo' className='footer__img' />
+          <p className='footer__title'>Quantovo</p>
+      </div>
+      <div className='footer__text'>
+        <p>© 2021 Quantovo Limited.</p>
+        <p>All rights reserved.</p>
+      </div>
+      <div className='footer__text'>
+        <p>Email</p>
+        <p>info@quantovo.com</p>
+      </div>
+     </FooterContainer>
+    </div>
   )
 }
