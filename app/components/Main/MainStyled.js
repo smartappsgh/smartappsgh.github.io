8c2edcf88c29eb46e82ef0e807f80786d1ee04ec
smartappsgh <smart.trader.dev@gmail.com>
2024-06-07 19:01:00 +0300

    make lines visible

--- app/components/Main/MainStyled.js
+++ app/components/Main/MainStyled.js
@@ -6,7 +6,7 @@ export const MainContainer = styled.div`
     flex-direction: column;
     align-items: center;
     text-align: center; 
-    height: 100vh;
+    height: 85vh;
     padding: 0 2rem;
     position: relative;
 
@@ -17,7 +17,7 @@ export const MainContainer = styled.div`
         width: 100%;
         height: 100%; 
         position: absolute;
-        bottom: 5%;
+        bottom: 10%;
         p {
             -moz-user-select: -moz-none;
             -o-user-select: none;
